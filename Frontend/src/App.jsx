7b30b888acbe2262daae9662114b2246c5c3937e
KrishKPs <kpskrishpatel@gmail.com>
2024-09-27 11:05:40 -0700

    Change2

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -16,7 +16,7 @@ import { AdminStatusChange } from './Pages/AdminStatusChange'
 import { Elements } from "@stripe/react-stripe-js";
 import { loadStripe } from "@stripe/stripe-js";
 import {  OrderHistroy } from './Pages/OrderHistory'
-import { PendingOrderCard } from './Components/PendingOrderCard'
+// import { PendingOrderCard } from './Components/PendingOrderCard'
 const stripePromise = loadStripe("pk_test_51Q3A8907tIc5oTXBtgselQRXk67WbbjMMM77V9AdHDqGhuAN7UpEZin4JZoZDDINt24NszTzqhBDidfP3iTBcaIM00f74WmCTg");
 
 function App() {
